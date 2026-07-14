cdab2c11e253efb10f2c464dbae1f927cb1fcca2
Fahad Illahi <fillahi@gmu.edu>
2026-07-14 01:41:57 +0000

    Add glance command

--- newfile.js
+++ newfile.js
@@ -11,10 +11,3 @@ console.log(lines.slice(0,5).join('\n'));
 console.log('...');
 console.log('Last 5:');
 console.log(lines.slice(-5).join('\n'));
-
-git init 
-git add newfile.js
-git commit -m "Add glance command"
-git branch -M main
-git branch add origin https://github.com/fillahi7861/fancy-glance-commands.git
-git push -u origin main
